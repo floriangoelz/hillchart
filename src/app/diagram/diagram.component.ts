f774e3266713d7c252e38997b1012292edb02da5
unknown <schms107@RM2MW055607.emea.roche.com>
2019-08-21 12:18:38 +0200

    footer + graph

--- src/app/diagram/diagram.component.ts
+++ src/app/diagram/diagram.component.ts
@@ -10,7 +10,7 @@ export class DiagramComponent implements OnInit {
   constructor() { }
 
   hill(x){
-    return 1/((Math.pow(x,2) + 1));
+    return 1/(0.05*(Math.pow(x,2) + 1));
   }
 
   drawDiagram() {
@@ -25,11 +25,11 @@ export class DiagramComponent implements OnInit {
       ctx.moveTo(10, 378);
       
       ctx.beginPath();
-      for(var i = -3.9; i <= 3.9; i = i + 0.01){
+      for(var i = -1.9; i <= 1.9; i = i + 0.01){
         var y = this.hill(i);
-        console.log((i + 4)*93.75, y);
-        ctx.lineTo((i + 4)*93.75, 400 - (350 * y));
-        //console.log(Math.round((i + 4)*93.75) + " " + Math.round(400 - (300 * y)))
+        console.log((i + 4)*187.5, y);
+        ctx.lineTo((i + 2)*187.5, 400 - (15 * y));
+        //console.log(Math.round((i + 4)*93.75) + " " + Math.round(400 - (60 * y)))
       }
       ctx.strokeStyle = "#06c";
       ctx.lineWidth = 4;
